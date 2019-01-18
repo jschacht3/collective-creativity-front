c771ed5a4963e3b153f8bff3fb10a3caf33f143f
Jason Schachter <jnschachter@gmail.com>
2019-01-18 15:34:37 -0500

    Removed: server files from boilerplate

---
+++ components/LinksView.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { StyleSheet, Image, Text, View } from 'react-native';
+import { WebBrowser } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import Touchable from 'react-native-platform-touchable';
+import Vote from './Vote'
+
+export default class LinksView extends React.Component {
+  render() {
+    return (
+      <View>
+        <Touchable
+          style={styles.option}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressVote} >
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                Vote!
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+
+        <Touchable
+          background={Touchable.Ripple('#ccc', false)}
+          style={styles.option}
+          onPress={this._handlePressSlack}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                Profile
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+
+        <Touchable
+          style={styles.option}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressDocs}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                How it works!
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+      </View>
+    );
+  }
+
+  _handlePressVote = () => {
+    console.log("HANDLING VOTE")
+    return (
+      <Vote/>
+    )
+  };
+
+  _handlePressForums = () => {
+    WebBrowser.openBrowserAsync('http://forums.expo.io');
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+  },
+  optionsTitleText: {
+    fontSize: 16,
+    marginLeft: 15,
+    marginTop: 9,
+    marginBottom: 12,
+  },
+  optionIconContainer: {
+    marginRight: 9,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
+  },
+  optionText: {
+    fontSize: 15,
+    marginTop: 1,
+  },
+});
---
+++ components/Vote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Icon } from 'expo';
+
+import Colors from '../constants/Colors';
+
+export default class Vote extends React.Component {
+  render() {
+    console.log("BLAH")
+    return (
+      <Text>Vote</Text>
+    );
+  }
+}
